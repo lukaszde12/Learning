e46d558e02c64f55aa9a11cf8a660662393fdfe7
Lukasz <lukaszderen@gmail.com>
2017-02-13 14:26:11 +0100

    Nauka

---
+++ Nauka/src/D0zw1l0n3.java
@@ -0,0 +1,8 @@
+public class D0zw1l0n3
+{
+	public static void main (String[] args)
+	{
+		System.out.println("Zaczynamy od litery, pozniej dozwolone cyfry i znaki specjalne"	);
+		System.out.println("Uwaga w nazwie class sa slowa zastrzezone jak public czy class, private etc."	);
+	}
+}
---
+++ Nauka/src/Zakuc.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ * automat tworzy komentarz
+ *
+ */
+
+public class Zakuc
+{
+	public static void main (String[] args)
+	{ 
+		System.out.println("Nie powiemy 'Witaj, swiecie!'");
+		
+		// komentarze klepac czasem trzeba! Pisze sie je od znaku do konca wiersza!
+		
+		/* komentarz 
+		 * w bloku tresci
+		 * moze miec pare linii
+		 */
+		
+		
+		/*
+		 * zmienne w Java
+		 * 
+		 * 
+		 * int (2mld cyfr 4 bajty)
+		 * short (32tys 2 bajty)
+		 * long (pierdyliard 8 bajtow)
+		 * byte (127 cyfr)
+		 * float (zmiennoprzecinkowe 6-7 cyfr, 4 bajty)
+		 * double (pierdyliard, 8 bajtow)
+		 * char (jednostka kodowa UTF-16)
+		 * boolean (true i false)
+		 * 
+		 * final (mozna tylko raz przypisac wartosc)
+		 */
+	}
+}
