a36a1fb69746b9cb1ff24af55f1b9c32b875be39
Lukasz <lukaszderen@gmail.com>
2017-02-23 12:49:06 +0100

    Add files via upload

---
+++ Nauka/src/TablicaPetla.java
@@ -0,0 +1,12 @@
+public class TablicaPetla {
+	public static void main (String args[]) {
+		int tab[] = new int[20];
+		for(int t = 0; t < 20; t++){
+			tab[t] = t + 1;}
+		int i = 0;
+			while(i < tab.length){
+				System.out.println("tab[" + i + "] = " + tab[i]); 
+				i++;
+}
+}
+}
---
+++ Nauka/src/tablica.java
@@ -0,0 +1,9 @@
+public class tablica {
+	public static void main (String args[]) {
+		int tab[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+			System.out.println("Zawartoœæ tablicy:");
+			for(int val: tab){
+				System.out.println(val);
+}
+}
+}
