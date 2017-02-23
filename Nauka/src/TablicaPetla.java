9c8db93e31347b8d182e58b5d5f990e1a03ded9f
Lukasz <lukaszderen@gmail.com>
2017-02-23 13:17:30 +0100

    Add files via upload

--- Nauka/src/TablicaPetla.java
+++ Nauka/src/TablicaPetla.java
@@ -10,3 +10,32 @@ public class TablicaPetla {
 }
 }
 }
+
+
+
+
+
+
+/* Tab ntego stopnia, wypelnic i zrobic kopiaTab ntego stopnia, skopiowac z Tab i odwrocic
+ * 
+ * Porzadek malejacy: 
+ * 
+  
+int tablica[] = new int[20];
+for(int i = 0; i < 20; i++){
+tablica[i] = 20 - i;
+}
+System.out.println("Zawartoœæ tablicy:");
+for(int i = 0; i < 20; i++){
+System.out.println("tablica[" + i + "] = " + tablica[i]);
+}
+
+
+Kopiowanie tablicy
+
+int[] kopiatab = Arrays.copyOf(tab, tab.length);
+
+ * 
+ * 
+ * 
+ */
